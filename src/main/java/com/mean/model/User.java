95b1d19d3f2b122b080814c81d073d556e6f936c
syedautherabbas <syedautherabbas@gmail.com>
2016-03-31 03:07:05 +0530

    Spr and hib to do heavylifts

---
+++ src/main/java/com/mean/model/User.java
@@ -0,0 +1,51 @@
+package com.mean.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String phone;
+	private String mail;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+}
--- src/main/java/com/mean/services/JSONService.java
+++ src/main/java/com/mean/services/JSONService.java
@@ -1,4 +1,4 @@
-package com.mkyong.rest;
+package com.mean.services;
 
 import java.util.ArrayList;
 
@@ -10,40 +10,37 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.mkyong.Track;
-import com.mkyong.UserObj;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.mean.model.User;
+import com.mean.services.UserService;
 
 
+@Controller
 @Path("/contactlist")
 public class JSONService {
 
+	@Autowired
+	private UserService userservice;
+	 public JSONService() {
+	System.out.println("JSON CONSTRUCTIOB");
+	}
+	
 	@GET
 	@Path("/get")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getuser() {
 
-		ArrayList<UserObj> list=new ArrayList<UserObj>();
-		UserObj u1=new UserObj();
-		u1.setName("ather");
-		u1.setMail("ather@gmail.com");
-		u1.setAge("23");
-		
-		UserObj u2=new UserObj();
-		u2.setName("rehan");
-		u2.setMail("rehan@gmail.com");
-		u2.setAge("24");
-		
-		UserObj u3=new UserObj();
-		u3.setName("kazim");
-		u3.setMail("kazim@gmail.com");
-		u3.setAge("32");
-		     
-		
-		
-		
-		list.add(u1);
-		list.add(u2);
-		list.add(u3);
+		try{
+			System.out.println("hi");
+		System.out.println(userservice.getusers());
+		}catch(Exception e)
+		{
+			  
+			e.printStackTrace();
+		}
 		 return Response
 		            .status(200)
 		            .header("Access-Control-Allow-Origin", "*")
@@ -51,17 +48,17 @@ public class JSONService {
 		            .header("Access-Control-Allow-Credentials", "true")
 		            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
 		            .header("Access-Control-Max-Age", "1209600")
-		            .entity(list)
+		            .entity("hi")
 		            .build();
 	}
 
 	@POST
 	@Path("/post")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createTrackInJSON(String name ) {
+	public Response createTrackInJSON(@ModelAttribute User user ) {
 
-		String result = "Track saved : " + name;
-		System.out.println(name+" TRACK");
+		String result = "User saved : " + user.getName();
+		System.out.println(result);
 		return Response.status(201).entity(result).build();
 		
 	}
